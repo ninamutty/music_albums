59a9c96e40a3ca195ae73e29f6b53df141d0ae94
Chris <chris@adadevelopersacademy.org>
2016-10-12 08:31:12 -0700

    Updated model testing slightly.

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -9,6 +9,8 @@ class ArtistTest < ActiveSupport::TestCase
     red_hot_chilli_peppers = Artist.new
 
     assert_not red_hot_chilli_peppers.valid? "An Artist without a name is invalid."
+    red_hot_chilli_peppers.name = "Konye"
+    assert red_hot_chilli_peppers.valid?  "Somehow Kanye is not a valid name."
   end
 
 
